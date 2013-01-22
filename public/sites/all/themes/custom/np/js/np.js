6db662a4eeace10a10edbe908e04a1cd953a9d01
Rob Hardwick <rob.hardwick@atchai.com>
2013-01-22 13:14:18 +0000

    Move event overlay CSS/JS into np theme

--- public/sites/all/themes/custom/np/js/np.js
+++ public/sites/all/themes/custom/np/js/np.js
@@ -114,6 +114,45 @@ Drupal.behaviors.np = {
     ----------------------------------------------------------------------------------------------------*/
     if (windowWidth >= mobileWidth) {
 
+      // Add event summary pop-up overlays
+      $('.event-status-summary-container').each(function(){
+        $(this).find('.event-status-link-container-overlay a.event-summary-link').qtip({
+          content: {
+            text: $(this).find('.event-status-content-container'),
+          },
+          position: {
+            my: 'top right',
+            at: 'bottom left',
+            target: $(this).find('.event-status-link-container-overlay a.event-summary-link')
+          },
+          show: {
+            event: 'click',
+            delay: 0,
+            solo: true,
+          },
+          hide: {
+            leave: false,
+            fixed: true,
+            delay: 1000,
+          },
+          events: {
+            show: function(event, api) {
+              // Doesn't appear to work for our purposes of preventing the original click event
+            }
+          },
+          style: {
+            def: false
+          }
+        });
+
+        // Using event.preventDefault() or event.originalEvent.preventDefault();
+        // in the qTip show event hook doesn't seem to work in stopping the
+        // click event. So we just do it here.
+        $(this).find('.event-status-link-container-overlay a.event-summary-link').click(function(e) {
+          e.preventDefault();
+        });
+      });
+
       // Select advert containers and all their children
       var $ads = $('.simplead-container'),
           $children = $ads.find('*');
